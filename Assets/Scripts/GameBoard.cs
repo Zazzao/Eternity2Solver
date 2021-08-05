0c87450cdc45e05a86a9b55c6b360ba815309ae4
Zazzao <61016432+Zazzao@users.noreply.github.com>
2021-08-05 16:27:42 -0400

    added basic game scripts

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour{
+
+    [SerializeField] private int boardSize = 25;
+
+
+
+
+
+}
